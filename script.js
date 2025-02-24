147ee8357cd4e079af2da16d72fbddccc00e9402
Lesya Had <elenaka7778@gmail.com>
2025-02-24 22:01:47 +0300

    Первые попытки

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ window.addEventListener('load', function () {
     canvas.width = 1500;
     canvas.height = 500;      
 })
+
+const game = new Game(canvas.width, canvas.height);
+
+    // animation loop
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Очищаем игровое поле перед следующей анимацией
+        game.draw(ctx);
+        game.update();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
