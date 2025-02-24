30d413a0a8335291e5020b19c61149152ac99775
Lesya Had <elenaka7778@gmail.com>
2025-02-24 22:30:19 +0300

    Изменения 3

--- src/Tank.js
+++ src/Tank.js
@@ -9,7 +9,14 @@ class Tank {
     }
 
     update() {
+        if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed
+        else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed
+        else this.speedY = 0;
         this.y += this.speedY;
+
+        if (this.y > this.game.height - this.height * 0.5) this.y = this.game.
+            height - this.height * 0.5;
+        else if (this.y < -this.height * 0.5) this.y = -this.height * 0.5;
     }
 
     draw(context) {
