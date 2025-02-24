85fb4657dccdececb8ab98c4cadcce5d594c556c
Lesya Had <elenaka7778@gmail.com>
2025-02-24 22:22:09 +0300

    Изменения 2

--- src/InputH.js
+++ src/InputH.js
@@ -2,8 +2,14 @@ class InputH {
     constructor(game) {
         this.game = game;
         window.addEventListener('keydown', (e) => {
-            if (((e.key === 'ArrowUp') || (e.key === 'ArrowDown')) && this.game.keys.indexOf(e.key) === -1) {
+            if (((e.key === 'ArrowUp') ||
+                (e.key === 'ArrowDown')
+            ) && this.game.keys.indexOf(e.key) === -1) {
                 this.game.keys.push(e.key);
+            } else if (e.key === ' ') {
+                this.game.player.shootTop();
+            } else if (e.key === 'd') {
+                this.game.debug = !this.game.debug;
             }
         });
         window.addEventListener('keyup', (e) => {
