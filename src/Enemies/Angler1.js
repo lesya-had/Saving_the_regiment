052994760bc138e17db86fdec36443ca626f4d5d
Lesya Had <elenaka7778@gmail.com>
2025-02-26 22:12:25 +0300

    Враги 1

--- src/Enemies/Angler1.js
+++ src/Enemies/Angler1.js
@@ -1,8 +1,8 @@
 class Angler1 extends Enemy {
     constructor(game) {
         super(game);
-        this.width = 228;
-        this.height = 169;
+        this.width = 228 * 0.5;
+        this.height = 169 * 0.5;
         this.y = Math.random() * (this.game.height * 0.95 - this.height);
         this.image = document.getElementById('angler1');
     }
