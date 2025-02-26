f08d5a2d6f3f2af51fae208607159b16f99a3b0c
Lesya Had <elenaka7778@gmail.com>
2025-02-26 21:56:26 +0300

    Враг1

---
+++ src/Enemies/Enemy.js
@@ -0,0 +1,17 @@
+class Enemy {
+    constructor(game) {
+        this.game = game;
+        this.x = this.game.width;
+        this.speedX = Math.random() * -1.5 - 2.5;
+        this.markedForDeletion = false;
+    }
+
+    update() {
+        this.x += this.speedX;
+        if (this.x + this.width < 0) this.markedForDeletion = true;
+    }
+
+    draw(context) {
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
